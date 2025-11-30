a5e0f62ae58d95bda24c582ced83d3d4eb87f2c2
RicardoChavira1 <rosquetobey@gmail.com>
2025-11-30 12:17:11 -0600

    Adhesión de código para conexión con base de datos
    Se añadieron dos líneas de código en el FireBase-admin para la conexión con la base de datos que creamos con firebase

--- lib/firebase-client.ts
+++ lib/firebase-client.ts
@@ -2,6 +2,7 @@
 import { getApps, initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,3 +18,4 @@ const firebaseConfig = {
 // Initialize Firebase
 export const app = getApps().length ? getApps()[0]! : initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
